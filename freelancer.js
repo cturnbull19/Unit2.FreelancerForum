e620807480492ec86ebe6eedba4fec5849baf7ba
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-27 17:20:49 -0500

    fixed selectedName and Occupation line and tried calculating average price

--- freelancer.js
+++ freelancer.js
@@ -1,24 +1,25 @@
 const names = ["Alice", "Bob", "Carol"];
 const occupations = ["Writer", "Teacher", "Programmer"];
-const prices = ["$30", "$50", "$70"]
-
+const prices = [30, 50, 70]
+const randNames = ["Annie", "Elizabeth", "Caroline", "Abby", "Santa", "MJ", "Sarah", "Joseph", "Sophie", "Douglas", "Anthony", "Dan"]
+const randOccupations = ["Geologist", "Talent Agent", "Software Developer", "Actress", "Lawyer", "Engineer", "Dancer", "Pilot", "Surfer", "Bull Rider"]
 //sample data for starter shapes
 const freelancers = [
     {
         name: "Alice",
         occupation: "Writer",
-        price: "$30"
+        price: 30
     }, 
     {
         name: "Bob",
         occupation: "Teacher",
-        price: "$50"
+        price: 50
     }
    
 ]
-const maxFreelancers = 20;
+const maxFreelancers = 10;
 
-const addFrelancerIntervalId = setInterval(addFrelancer, 3000)
+const addFreelancerIntervalId = setInterval(addFreelancer, 3000)
 
 //render()
 
@@ -38,6 +39,7 @@ const addFrelancerIntervalId = setInterval(addFrelancer, 3000)
 
 function render(){
     const freelancerContainer = document.querySelector("#freelancers");
+    const averagePriceElement = document.querySelector("#average");
     console.log(freelancerContainer)
 
     const freelancerElements = freelancers.map((freelancer) => {
@@ -62,17 +64,21 @@ function render(){
     });
 
     freelancerContainer.replaceChildren(...freelancerElements);
+    const totalPrices = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0);
+    const averagePrice = freelancers.length > 0 ? (totalPrices / freelancers.length).toFixed(2) : 0;
+    averagePriceElement.textContent = averagePrice;
 }
 function addFreelancer() {
-    const selectedName = names[Math.floor(Math.random() * names.length)]
-    const selectedOccupation = occupations[Math.floor(Math.random() * occupations.length)]
-    const selectedPrice = prices[Math.floor(Math.random() * prices.length)]
+    const selectedName = randNames[Math.floor(Math.random() * randNames.length)]
+    const selectedOccupation = randOccupations[Math.floor(Math.random() * randOccupations.length)]
+    const selectedPrice = "$" + Math.floor(Math.random() * 100)
 
-    freelancers.push({selectedName, selectedOccupation, selectedPrice})
+    freelancers.push({name: selectedName, occupation: selectedOccupation, price: selectedPrice})
 
     if (freelancers.length >= maxFreelancers) {
-        clearInterval(addFrelancerIntervalId)
+        clearInterval(addFreelancerIntervalId)
     }
 
     render()
 }
+
